b9ba5cc0a5857b78d6993dea229f5045e7d4c945
Erik Higginbotham <erikhigginbotham@fullstory.com>
2023-12-07 14:51:07 -0500

    Day 7 Part 2 done

--- 2023/day7/index.ts
+++ 2023/day7/index.ts
@@ -1,4 +1,4 @@
-import { readFile, print, extract } from '../utils';
+import { readFile } from '../utils';
 
 interface HandMap {
   hand: string;
@@ -14,11 +14,9 @@ function parseInput() {
   return mapped;
 }
 
-export function pt1() {
-  const mapped = parseInput();
-
+function doWork(mapped: HandMap[], part2: boolean = false) {
   const res = mapped.map((map) => {
-    return { strength: checkHand(map.hand), bid: map.bid };
+    return { strength: checkHand(map.hand, part2), bid: map.bid };
   });
   res.sort((a, b) => {
     if (a.strength[0] !== b.strength[0]) return b.strength[0] - a.strength[0];
@@ -29,7 +27,25 @@ export function pt1() {
     }
     return CARD_STRENGTH[b.strength[1][i]] - CARD_STRENGTH[a.strength[1][i]];
   });
+  return res;
+}
 
+export function pt1() {
+  const mapped = parseInput();
+  const res = doWork(mapped);
+  const ret = res.map((r, i) => {
+    return { ...r, rank: res.length - i };
+  });
+  let sum = 0;
+  for (let i = 0; i < ret.length; i++) {
+    sum += ret[i].bid * ret[i].rank;
+  }
+  return sum;
+}
+
+export function pt2() {
+  const mapped = parseInput();
+  const res = doWork(mapped, true);
   const ret = res.map((r, i) => {
     return { ...r, rank: res.length - i };
   });
@@ -41,6 +57,7 @@ export function pt1() {
 }
 
 const CARD_STRENGTH: { [key: string]: number } = {
+  J: 0,
   '2': 1,
   '3': 2,
   '4': 3,
@@ -50,10 +67,9 @@ const CARD_STRENGTH: { [key: string]: number } = {
   '8': 7,
   '9': 8,
   T: 9,
-  J: 10,
-  Q: 11,
-  K: 12,
-  A: 13,
+  Q: 10,
+  K: 11,
+  A: 12,
 };
 
 const HAND_TO_STRENGTH = {
@@ -66,13 +82,24 @@ const HAND_TO_STRENGTH = {
   FIVE_OF_A_KIND: 7,
 };
 
-function checkHand(hand: string): [number, string] {
+function checkHand(hand: string, part2: boolean = false): [number, string] {
   const cardMap = new Map<string, number>();
   for (const card of hand) {
     if (cardMap.has(card)) {
       cardMap.set(card, cardMap.get(card)! + 1);
     } else {
       cardMap.set(card, 1);
+    }
+  }
+  if (part2) {
+    if (cardMap.has('J')) {
+      const jVal = cardMap.get('J')!;
+      cardMap.delete('J');
+      const maxVal = Math.max(...[...cardMap.values()]);
+      const maxKey = [...cardMap.keys()].find(
+        (k) => cardMap.get(k) === maxVal
+      )!;
+      cardMap.set(maxKey, maxVal + jVal);
     }
   }
 
--- 2023/index.ts
+++ 2023/index.ts
@@ -4,7 +4,7 @@ import { pt1 as day3pt1, pt2 as day3pt2 } from './day3';
 import { pt1 as day4pt1, pt2 as day4pt2 } from './day4';
 import { pt1 as day5pt1, pt2 as day5pt2 } from './day5';
 import { pt1 as day6pt1, pt2 as day6pt2 } from './day6';
-import { pt1 as day7pt1 } from './day7';
+import { pt1 as day7pt1, pt2 as day7pt2 } from './day7';
 
 const solutions = {
   day1Pt1: `Day 1 Part 1: ${day1pt1()}`,
@@ -20,6 +20,7 @@ const solutions = {
   day6pt1: `Day 6 Part 1: ${day6pt1()}`,
   day6pt2: `Day 6 Part 2: ${day6pt2()}`,
   day7pt1: `Day 7 Part 1: ${day7pt1()}`,
+  day7pt2: `Day 7 Part 2: ${day7pt2()}`,
 };
 
 for (const value of Object.values(solutions)) {
