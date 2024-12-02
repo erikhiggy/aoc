21ad837176b07bcb329d0546e2aaa7a3c79fe796
Erik Higginbotham <erikhigginbotham@fullstory.com>
2024-12-02 15:16:39 -0500

    day02 part 2 done.

--- 2024/day02/main.go
+++ 2024/day02/main.go
@@ -64,44 +64,32 @@ func main() {
 	// For each list, we want to detect if it is valid. It can only be valid if the following two constraints are met:
 	// 1. The list must be either all increasing or all decreasing.
 	// 2. ny two adjacent numbers differ by at least one and at most three.
-	validReports := make([]string, len(in))
-	// first check if the list is increasing or decreasing
-	for idx, list := range in {
-		inc, dec := true, true
-		for i := 1; i < len(list); i++ {
-			if list[i] > list[i-1] {
-				dec = false
-			} else if list[i] < list[i-1] {
-				inc = false
-			}
-		}
-
-		if !inc && !dec {
-			validReports[idx] = "unsafe"
-			fmt.Println("invalid due to increasing/decreasing")
-			continue
-		}
-		// next check that the difference between adjacent numbers is at least one and at most three
-		for i := 1; i < len(list); i++ {
-			if abs(list[i]-list[i-1]) > 3 || abs(list[i]-list[i-1]) < 1 {
-				validReports[idx] = "unsafe"
-				fmt.Println("invalid due to difference")
-				break
-			}
-		}
-		if validReports[idx] == "" {
-			validReports[idx] = "safe"
-		}
-	}
-
-	// sum the number of safe lists
 	safeCount := 0
-	for _, report := range validReports {
-		if report == "safe" {
+	// first check if the list is increasing or decreasing
+	for _, list := range in {
+		if isSafe(list) {
 			safeCount++
 		}
 	}
+
 	fmt.Println("Safe lists:", safeCount)
+
+	// Part 2
+	newSafeCount := 0
+	for _, list := range in {
+		if isSafe(list) {
+			newSafeCount++
+		} else {
+			for i := 0; i < len(list); i++ {
+				removed := removeIdx(list, i)
+				if isSafe(removed) {
+					newSafeCount++
+					break
+				}
+			}
+		}
+	}
+	fmt.Println("New safe lists:", newSafeCount)
 }
 
 func abs(x int) int {
@@ -109,4 +97,37 @@ func abs(x int) int {
 		return -x
 	}
 	return x
+}
+
+func isSafe(list []int) bool {
+	inc, dec := true, true
+	for i := 1; i < len(list); i++ {
+		if list[i] > list[i-1] {
+			dec = false
+		} else if list[i] < list[i-1] {
+			inc = false
+		}
+	}
+
+	if !inc && !dec {
+		return false
+	}
+
+	for i := 1; i < len(list); i++ {
+		if abs(list[i]-list[i-1]) > 3 || abs(list[i]-list[i-1]) < 1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func removeIdx(list []int, idx int) []int {
+	result := make([]int, 0, len(list)-1)
+	for i, num := range list {
+		if i != idx {
+			result = append(result, num)
+		}
+	}
+	return result
 }
